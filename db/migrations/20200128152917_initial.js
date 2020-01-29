8a655459469279532f89b2159be2a0727ada1de0
Nick Nist <nicknistoc@gmail.com>
2020-01-28 18:27:43 -0700

    Add intitial migration. Creates matches table and teams table

--- db/migrations/20200128152917_initial.js
+++ db/migrations/20200128152917_initial.js
@@ -1,8 +1,33 @@
 
-exports.up = function(knex) {
-  
+exports.up = knex => {
+  return knex.schema
+    .createTable('teams', table => {
+      table.increments('id').primary();
+      table.string('name');
+      table.string('league');
+      table.integer('rank').unsigned();
+      table.decimal('spi');
+
+      table.timestamps(true, true);
+    })
+    .createTable('matches', table => {
+      table.increments('id').primary();
+      table.string('date');
+      table.string('team1_name');
+      table.integer('team1_id').unsigned();
+      table.foreign('team1_id').references('teams.id');
+      table.integer('team1_score').unsigned();
+      table.string('team2_name');
+      table.integer('team2_id').unsigned();
+      table.foreign('team2_id').references('teams.id');
+      table.integer('team2_score').unsigned();
+
+      table.timestamps(true, true);
+    })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+    .dropTable('matches')
+    .dropTable('teams')
 };
