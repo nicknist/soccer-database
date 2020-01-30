cec201f21e3e020dab6078d9de45a84c10ca3399
Nick Nist <nicknistoc@gmail.com>
2020-01-30 15:32:38 -0700

    Add get and post endpoints

--- server.js
+++ server.js
@@ -24,7 +24,82 @@ app.get('/api/v1/teams', async (request, response) => {
 app.get('/api/v1/matches', async (request, response) => {
   try {
     const matches = await database('matches').select();
-    response.status(500).json(matches);
+    response.status(200).json(matches);
+  } catch (error) {
+    response.status(500).json({ error });
+  }
+});
+
+app.get('/api/v1/teams/:id', async (request, response) => {
+  try {
+    const team = await database('teams').where({ id: request.params.id })
+    if (!team[0]) {
+      response.status(404).json({ error: 'Team not found' });
+    } else {
+      response.status(201).json(team[0]);
+    }
+  } catch (error) {
+    response.status(500).json({ error });
+  }
+});
+
+app.get('/api/v1/matches/:id', async (request, response) => {
+  try {
+    const match = await database('matches').where({ id: request.params.id });
+    if (!match[0]) {
+      response.status(404).json({ error: 'Match not found' });
+    } else {
+      response.status(201).json(match[0]);
+    }
+  } catch (error) {
+    response.status(500).json({ error });
+  }
+});
+
+app.post('/api/v1/teams', async (request, response) => {
+  const team = request.body
+
+  for (let requiredParameter of ['name', 'league', 'rank', 'spi']) {
+    if (!team[requiredParameter]) {
+      return response
+        .status(422)
+        .send({ error: `Expected format: {
+          name: <String>,
+          league: <String>,
+          rank: <Number>,
+          spi: <Number>
+        }. You're missing a "${requiredParameter}" property.`})
+    }
+  }
+
+  try {
+    const id = await database('teams').insert(team, 'id');
+    response.status(201).json({ id: id[0] });
+  } catch (error) {
+    response.status(500).json({ error });
+  }
+});
+
+app.post('/api/v1/matches', async (request, response) => {
+  const match = request.body;
+
+  for (let requiredParameter of ['date', 'team1_name', 'team1_score', 'team2_name', 'team2_score']) {
+    if (!match[requiredParameter]) {
+      return response
+      .status(422)
+      .send({ error: `Expected format: {
+        date: <String>,
+        team1_name: <String>,
+        team1_score: <String>,
+        team2_name: <String>,
+        team2_score: <String>
+      }. You're missing a "${requiredParameter}" property.`})
+    }
+  }
+
+  try {
+    const id = await database('matches').insert(match, 'id');
+    response.status(201).json({ id: id[0] });
   } catch (error) {
     response.status(500).json({ error });
   }
