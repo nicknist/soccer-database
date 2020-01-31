959b060764ec1482649672a9adc2de7258527ed8
Nick Nist <nicknistoc@gmail.com>
2020-01-31 09:24:47 -0700

    Add Procfile and update knexfile production

--- knexfile.js
+++ knexfile.js
@@ -12,4 +12,12 @@ module.exports = {
     },
     useNullAsDefault: true,
   },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
+  }
 };
